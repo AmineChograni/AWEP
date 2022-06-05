95d15725daec5578b31608602004a5b534b3a9b1
Amine CHOGRANI <aminechograni@gmail.com>
2022-06-05 14:21:34 +0100

    configur routing

--- AWEPFE/src/app/static/static-routing.module.ts
+++ AWEPFE/src/app/static/static-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StaticComponent } from './static.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: StaticComponent,
+    children: [
+      { path: "", loadChildren: () => import('./home/home.module').then(mod => mod.HomeModule) }
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
