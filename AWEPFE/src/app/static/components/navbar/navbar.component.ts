1e58a3e3c47b898e27a5695903f28b33936afa6a
Amine CHOGRANI <aminechograni@gmail.com>
2022-06-07 18:56:06 +0100

    navbar v1

--- AWEPFE/src/app/static/components/navbar/navbar.component.ts
+++ AWEPFE/src/app/static/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from "jquery";
 
 @Component({
   selector: 'app-navbar',
@@ -8,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 export class NavbarComponent implements OnInit {
 
   constructor() { }
-
   ngOnInit(): void {
 
+    $(".menu-toggle-btn").click(function(){
+      $(this).toggleClass("fa-times");
+      $(".navigation-menu").toggleClass("active");
+    });
 
   }
 
