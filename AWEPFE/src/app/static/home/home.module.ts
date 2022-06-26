eda8855a12dfa3c17d969c5bdf3059a7b0d5e1b3
Amine CHOGRANI <aminechograni@gmail.com>
2022-06-26 19:48:41 +0100

    generate articles section

--- AWEPFE/src/app/static/home/home.module.ts
+++ AWEPFE/src/app/static/home/home.module.ts
@@ -14,11 +14,12 @@ import { NzGridModule } from 'ng-zorro-antd/grid';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzCardModule } from 'ng-zorro-antd/card';
+import { ArticlesSectionComponent } from './articles-section/articles-section.component';
 
 
 
 @NgModule({
-  declarations: [HomeComponent, HeroSectionComponent, EncoreSectionComponent, SommeSectionComponent, DiplomesSectionComponent],
+  declarations: [HomeComponent, HeroSectionComponent, EncoreSectionComponent, SommeSectionComponent, DiplomesSectionComponent, ArticlesSectionComponent],
   imports: [
     CommonModule,
     HomeRoutingModule,
